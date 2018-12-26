9dd617ce8b0a58b38998170b69cc8dca905e7ef0
InsignificantSpeck <theczaar@gmail.com>
2018-12-26 09:50:39 -0600

    added comments to hello/main.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main()
+//declares function called int main which accepts no arguments.
+//main is the point at which this program starts at.
 {
-	std::cout << "hello world" <<endl;
-
+	std::cout << "hello world" <<std::endl;
+// cout enables the ability to print using the ostream and
+// endl allows us to move to the next line.
 return 0;
+//return 0 enables the ability to complete.
 }
